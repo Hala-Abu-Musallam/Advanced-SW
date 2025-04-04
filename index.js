a9dd14883338b22096f9393b69c069a63073d38a
Hala-Abu-Musallam <halakhaled2002g2@gmail.com>
2025-04-04 15:24:15 +0300

    initial program

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8081; // Define PORT here
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express.js!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
