81e83ef0fb2d31e097e155569658145e5b9a6487
Rawan-Ghassan <s12112097@stu.najah.edu>
2025-05-20 16:27:13 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const path = require('path');
 
-// ✅ استدعاء ملف الاتصال من مجلد أعلى
 const sequelize = require(path.join(__dirname, '..', 'database'));
 
 const db = {};
@@ -11,7 +10,7 @@ db.sequelize = sequelize;
 db.Trust = require('./trust')(sequelize, DataTypes);
 db.Review = require('./review')(sequelize, DataTypes);
 
-// ✅ العلاقات
+
 db.Trust.hasMany(db.Review, { foreignKey: 'trustId', onDelete: 'CASCADE' });
 db.Review.belongsTo(db.Trust, { foreignKey: 'trustId' });
 
